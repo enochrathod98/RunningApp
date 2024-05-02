0103b115596f86d973facffc92791613f6bf5a74
knoxpo <enochrathod98@gmail.com>
2024-05-02 18:31:29 +0530

    Setting Up Architectural Structure

---
+++ app/src/main/java/com/example/runningapp/repositories/MainRepository.kt
@@ -0,0 +1,31 @@
+package com.example.runningapp.repositories
+
+import com.example.runningapp.db.Run
+import com.example.runningapp.db.RunDAO
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    val runDao: RunDAO
+) {
+    suspend fun insertRun(run: Run) = runDao.insertRun(run)
+
+    suspend fun deleteRun(run: Run) = runDao.deleteRun(run)
+
+    fun getAllRunSortedByDate() = runDao.getAllRunsSortedByDate()
+
+    fun getAllRunsSortedByTimeInMillis() = runDao.getAllRunsSortedByTimeInMillis()
+
+    fun getAllRunsSortedByCaloriesBurned() = runDao.getAllRunsSortedByCaloriesBurned()
+
+    fun getAllRunsSortedByAvgSpeed() = runDao.getAllRunsSortedByAvgSpeed()
+
+    fun getAllRunsSortedByDistance() = runDao.getAllRunsSortedByDistance()
+
+    fun getTotalTimeInMillis() = runDao.getTotalTimeInMillis()
+
+    fun getTotalCaloriesBurned() = runDao.getTotalCaloriesBurned()
+
+    fun getTotalDistance() = runDao.getTotalDistance()
+
+    fun getTotalAvgSpeed() = runDao.getTotalAvgSpeed()
+}
--- app/src/main/java/com/example/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/runningapp/ui/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.example.runningapp
+package com.example.runningapp.ui
 
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.runningapp.R
 import com.example.runningapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
